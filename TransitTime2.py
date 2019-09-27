173cfd38143d4df926df62f4a5ee4ec50b065c91
VoxleJ <45022791+VoxleJ@users.noreply.github.com>
2019-09-27 15:32:56 -0400

    Update TransitTime2.py

--- TransitTime2.py
+++ TransitTime2.py
@@ -50,6 +50,7 @@ third = csv1[2,:]
 #print(csv1[:,1])
 xraw1 = csv1[:,0]
 yraw1 = csv1[:,1]
+#yraw1 = yraw1 - 2000 #for different data
 #plt.plot(xraw1,yraw1)
 
 filename2 = 'HD_descend.csv'
@@ -59,6 +60,8 @@ third2 = csv2[2,:]
 
 xraw2 = csv2[:,0]
 yraw2 = -1*csv2[:,1]
+#yraw2 = csv2[:,1]
+#yraw2 = -1*(yraw2-2000)
 #plt.plot(xraw2,yraw2)
 #plt.show()
 
@@ -73,35 +76,86 @@ yraw2 = -1*csv2[:,1]
 
 
 
-f1 = interp1d(csv1[:,0], csv1[:,1], kind='cubic') #interpolation to 1 sample/ms
-f2 = interp1d(csv2[:,0], yraw2, kind='cubic')#csv2[:,1], kind='cubic')
+#alternate interpolation method
+#f1 = InterpolatedUnivariateSpline(xraw1, yraw1)
+#f2 = InterpolatedUnivariateSpline(xraw2,yraw2)
+
 ab = max(csv1[:,0])
 abc = 1000 * (ab + 1)
-csvxnew = np.linspace(0, max(csv1[:,0]), num=733000, endpoint=True) #adjust x scale to accomodate
+abcd = int(abc)
+csvxnew = np.linspace(0, max(csv1[:,0]), abcd, endpoint=True) #adjust x scale to accomodate
 
 #csvxnew = np.arange(abc)
 
 x = csv1[:,0]
 y = csv1[:,1]
+yraw3 = [0] * len(yraw1)
+yraw4 = [0] * len(yraw2)
+#y3 = [0] * len(y1)
+#y4 = [0] * len(y2)
+
+for i in range(len(yraw1)):
+    yraw3[i] = (yraw1[i] - min(yraw1)) / (max(yraw1) - min(yraw1))
+    
+print("Checkpoint1")
+t1 = time.time()
+total = t1-t0
+print('Time elapsed: ', total, 's')
+
+for i in range(len(yraw2)):
+    yraw4[i] = (yraw2[i] - min(yraw2)) / (max(yraw2) - min(yraw2))
+
+print("Checkpoint2")
+t1 = time.time()
+total = t1-t0
+print('Time elapsed: ', total, 's')
+
+f1 = interp1d(xraw1, yraw3, kind='cubic') #interpolation to 1 sample/ms
+f2 = interp1d(xraw2, yraw4, kind='cubic')#csv2[:,1], kind='cubic')
+
+print("Checkpoint3")
+t1 = time.time()
+total = t1-t0
+print('Time elapsed: ', total, 's')
 
 plt.figure(2)
-plt.plot(xraw1, yraw1, 'o', csvxnew, f1(csvxnew), '--')
-print("Checkpoint1")
-plt.plot(xraw2, yraw2, 'o', csvxnew, f2(csvxnew), '--')
+plt.plot(xraw1, yraw3, 'o', csvxnew, f1(csvxnew), '--')
+print("Checkpoint4")
+
+plt.plot(xraw2, yraw4, 'o', csvxnew, f2(csvxnew), '--')
+
 #plt.show()
+
 
 y1 = f1(csvxnew)
 y2 = f2(csvxnew)
 
+#normalizing
+
+t1 = time.time()
+total = t1-t0
+print('Time elapsed: ', total, 's')
+
+    
+
 #Running xcorr
-plt.figure(3)
-print("Checkpoint2")
+plt.figure(5)
+
+print("Checkpoint5")
 lags = plt.xcorr(y1, y2, usevlines=False,  maxlags=None, normed=True, lw=2)
-print("Checkpoint3")
+t1 = time.time()
+total = t1-t0
+print('Time elapsed: ', total, 's')
+
+print("Checkpoint6")
 plt.figure(6)
-c = plt.xcorr(yraw1, yraw2, usevlines=True, maxlags=None, normed=False, lw=2)
+c = plt.xcorr(yraw3, yraw4, usevlines=True, maxlags=None, normed=True, lw=2)
+t1 = time.time()
+total = t1-t0
+print('Time elapsed: ', total, 's')
+
 #plt.show()
-print("Checkpoint4")
+print("Checkpoint7")
 
 #xcorr_array = np.correlate(y1,y2, 'full')
 #peaks,_ = find_peaks(xcorr_array)
@@ -128,6 +182,7 @@ for i in range(len(peaks2)):
     print('Interpolation Peakx', i, ': ', xloc_xcorr[peaks2[i]])
     print()
     
+print(filename1)
 #plt.figure(7)
 
 #csvxnew34 = np.linspace(0, 1465999, num=1465999, endpoint=True)
@@ -161,18 +216,13 @@ for i in range(len(peaks2)):
 # unraph = np.unwrap(angh)
 # =============================================================================
 
-
-
-
-
-
 #corr = signal.correlate(csv1,csv2, mode='full', method='auto')
 #plt.figure(5)
 #plt.plot(corr)
 #HD Peaks 379387 1006257 1416870
 #PJ 218165 412034 727389 1068257 1293405 1438166
 t1 = time.time()
-total = t1-t0
-print('Time elapsed: ', total)
-
+total = (t1-t0)/60
+rtotal = round(total, 2)
+print('Total Time elapsed: ', rtotal, 'min')
 
