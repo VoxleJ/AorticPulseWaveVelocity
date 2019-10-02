3843d39351aab969dd07a77c77300bb777efb468
VoxleJ <45022791+VoxleJ@users.noreply.github.com>
2019-10-02 16:41:07 -0400

    Add files via upload

---
+++ 3D Checking.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Oct  1 14:06:52 2019
+
+@author: vjha1
+"""
+
+import matplotlib.pyplot as plt
+import numpy as np
+from mpl_toolkits.mplot3d import Axes3D
+
+filename1 = 'McCoskeyD_distance.csv'
+csv1 = np.genfromtxt (filename1, delimiter=",")
+
+fig = plt.figure()
+ax = fig.gca(projection='3d')
+
+xraw1 = csv1[:,0]
+yraw1 = csv1[:,1]
+zraw1 = csv1[:,2]
+
+plt.plot(xraw1, yraw1, zraw1 ,  lw = 10)
+plt.show()
